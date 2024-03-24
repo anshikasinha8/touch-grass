07c5517abdcfaca5f806d18792b1489653274ce8
ch103hw4n6 <ch0254@uah.edu>
2024-03-23 23:21:01 -0500

    front-end

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />}/>
-            <Route path="/" element={<Map />}/>
+            <Route path="/map" element={<Map />}/>
           </Routes>
       </BrowserRouter>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import "./Navbar.css";
 export const Navbar = () => {
     return (
         <div className="nav">
-            <Link to="/" className="title"> Home </Link>
-            <Link to="/map" className="title"> Map </Link>
+            <Link to="/" className="title"> HOME </Link>
+            <Link to="/map" className="title"> MAP </Link>
         </div>
     );
 };
