c6c4d74f1b13d9835ecb155d801fd2e6d3f93286
kmashley <kmashley706@gmail.com>
2024-03-23 21:12:47 -0500

    Added all polygons, comments, polygon pop up, image urls, all coordinates for polygons

--- src/components/pages/Map.jsx
+++ src/components/pages/Map.jsx
@@ -3,6 +3,8 @@ import { MapContainer, TileLayer, Marker, Popup, Polygon } from 'react-leaflet';
 import Dropdown from '../Dropdown';
 
 export const Map = () => {
+
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221707898159114/IMG_2512.jpg?ex=6611ca58&is=65ff5558&hm=1880164f96e168d83128c7b008cce2ff5d70da17e37ecfd68e09ccc5d0cc84ae&=&format=webp&width=1038&height=778*/
   const sstTreePoly = [
     [34.725967, -86.640218],
     [34.726026, -86.639985],
@@ -10,6 +12,7 @@ export const Map = () => {
     [34.725781, -86.640044]
   ];
 
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221136582866894909/IMG_2466.jpg?ex=66117b10&is=65ff0610&hm=16066258af13664290d230ab8a2045afc48787e6ceec0224ac733d83331b724e&=&format=webp&width=1038&height=778 */
   const greenHouse = [
     [34.725875, -86.641028],
     [34.725983, -86.640863],
@@ -17,6 +20,7 @@ export const Map = () => {
     [34.725723, -86.640886]
   ];
 
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221219977152172052/IMG_6570.jpg?ex=6611c8bb&is=65ff53bb&hm=65fdfedf29be1918835b848f1d3d49bfe17bfd6d7e7945d9bb2939691bdb38ed&=&format=webp&width=1038&height=778*/
   const bbTheatre = [
     [34.727071, -86.639532],
     [34.727025, -86.639390],
@@ -24,15 +28,17 @@ export const Map = () => {
     [34.726913, -86.639546]
   ];
 
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221219976384348385/IMG_6572.jpg?ex=6611c8bb&is=65ff53bb&hm=822882312e5fa80d510dee1da8480558d41d8a46d2c5d3d7ec62a73c1416a49a&=&format=webp&width=1038&height=778*/
   const sstBalcony = [
     [34.725958, -86.641453],
     [34.725920, -86.641658],
     [34.725836, -86.641836],
     [34.725556, -86.641671],
     [34.725613, -86.641394],
-    [34.725798, -86.641282],
+    [34.725798, -86.641282]
   ];
 
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221419229253712/IMG_9336.jpg?ex=6611ca13&is=65ff5513&hm=c5de7e92a4c6a3ccf484f2e0d363cf8f13f3936aeb5f3a0367fe11ff4edb66d2&=&format=webp&width=1038&height=778*/
   const mortonCourt = [
     [34.732583, -86.641278],
     [34.732407, -86.640949],
@@ -42,6 +48,7 @@ export const Map = () => {
     [34.732380, -86.641635]
   ];
 
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221417136422952/IMG_9334.jpg?ex=6611ca13&is=65ff5513&hm=7b5bee9835f04e9fa530d8798012be1ad7c33394556d78b20f51c93cd28e85d5&=&format=webp&width=1038&height=778*/
   const mortonBench = [
     [34.733048, -86.641690],
     [34.733158, -86.641528],
@@ -50,6 +57,7 @@ export const Map = () => {
     [34.732589, -86.641226]
   ];
 
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221419971641524/IMG_9337.jpg?ex=6611ca13&is=65ff5513&hm=4f9fe3502de9a27b2492734932601824a3915dceda4d40b27b77613c7313b4b4&=&format=webp&width=1038&height=778*/
   const cuLawn = [
     [34.727331, -86.640132],
     [34.727500, -86.640235],
@@ -61,6 +69,7 @@ export const Map = () => {
     [34.727373, -86.639655]
   ];
 
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221409918029937/IMG_2469.jpg?ex=6611ca11&is=65ff5511&hm=d5d98ee6eb9bbaa0ed236312109bbd310cd0df9c2494a967a9a27c132cd9ea65&=&format=webp&width=583&height=778*/
   const salmonForest = [
     [34.728374, -86.640442],
     [34.728374, -86.640158],
@@ -71,6 +80,7 @@ export const Map = () => {
     [34.728406, -86.640532]
   ];
 
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221411612397678/IMG_2470.jpg?ex=6611ca11&is=65ff5511&hm=636fe1eeb1cc7a62fd84111f556988470df0bf7d2db13c16d4834dbb4d2df67e&=&format=webp&width=583&height=778*/
   const salmonSlab = [
     [34.728808, -86.640068],
     [34.728660, -86.639733],
@@ -79,6 +89,7 @@ export const Map = () => {
     [34.728692, -86.640351]
   ];
 
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221415290802236/IMG_2474.jpg?ex=6611ca12&is=65ff5512&hm=e1294cd5bcec343442de5ca994adb4257182b18c77e4f9e8088c2aeaeaf3095f&=&format=webp&width=1038&height=778*/
   const nursingBench = [
     [34.729048, -86.639358],
     [34.729048, -86.639120],
@@ -90,6 +101,7 @@ export const Map = () => {
     [34.729561, -86.639010]
   ];
 
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221416398098472/IMG_2475.jpg?ex=6611ca12&is=65ff5512&hm=2f48dd4a0c575f448a9184024da1eb1e30f7a9a1c9ddc5ff8e03798fb968883a&=&format=webp&width=1038&height=778*/
   const salmonBench1 = [
     [34.729460, -86.639705],
     [34.729454, -86.639899],
@@ -97,6 +109,7 @@ export const Map = () => {
     [34.729619, -86.639692]
   ];
 
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221418994503781/IMG_2479.jpg?ex=6611ca13&is=65ff5513&hm=824da2ad071165e2ac42b5b366d11b42f33c25938915d1584e5cd2f2953c6b89&=&format=webp&width=1038&height=778*/
   const salmonBench2 = [
     [34.729756, -86.639725],
     [34.729762, -86.639950],
@@ -105,6 +118,7 @@ export const Map = () => {
     [34.729841, -86.639731]
   ];
 
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221420323967046/IMG_2480.jpg?ex=6611ca13&is=65ff5513&hm=089a6cfe54ecaa5376023e16f6061390e599d22bc84a424312c632e65593092c&=&format=webp&width=1038&height=778*/
   const artStructures = [
     [34.730984, -86.639831],
     [34.731298, -86.640305],
@@ -112,6 +126,7 @@ export const Map = () => {
     [34.731598, -86.639602]
   ];
 
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221423062847599/IMG_2482.jpg?ex=6611ca14&is=65ff5514&hm=8ef0346c36a561055021bc594a623fd8d9063aef1bc50057e309106662a007c2&=&format=webp&width=1038&height=778*/
   const bigTree = [
     [34.731286, -86.640322],
     [34.730913, -86.639909],
@@ -120,6 +135,7 @@ export const Map = () => {
     [34.731230, -86.640448]
   ];
 
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221583046312057/IMG_2486.jpg?ex=6611ca3a&is=65ff553a&hm=8a9ea1fbd4d3640ec4aea3894b2a2637d625d8c877882c4819871efe2d33c136&=&format=webp&width=1038&height=778*/
   const franzBench = [
     [34.733385, -86.641010],
     [34.733067, -86.640680],
@@ -127,6 +143,7 @@ export const Map = () => {
     [34.733280, -86.641180]
   ];
 
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221585466167476/IMG_2488.jpg?ex=6611ca3b&is=65ff553b&hm=b3ad9c5617191d42e5e1091e2ebfa670e1985c3036146695f91d6f7b177b8ff1&=&format=webp&width=1038&height=778*/
   const mortonSlab = [
     [34.732327, -86.641605],
     [34.732214, -86.641612],
@@ -137,12 +154,152 @@ export const Map = () => {
     [34.732560, -86.641818]
   ];
 
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221587706187866/IMG_2491.jpg?ex=6611ca3b&is=65ff553b&hm=8df0ba15126498fe51b51f95121da725833af24091b4780392bd8f1bb1f382a3&=&format=webp&width=1038&height=778*/
   const oldPlayground = [
     [34.731756, -86.642227],
     [34.731744, -86.641965],
     [34.731622, -86.641844],
     [34.731467, -86.641926],
     [34.731529, -86.642206]
+  ];
+
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221588444123217/IMG_2492.jpg?ex=6611ca3b&is=65ff553b&hm=2ee4c77f0668d4dc489d6b2da6a2833b1189da30f3b5cc783b4662e947c17f19&=&format=webp&width=583&height=778*/
+  const ctcTables = [
+    [34.731350, -86.641956],
+    [34.731527, -86.641655],
+    [34.731286, -86.641548],
+    [34.731225, -86.641577],
+    [34.731150, -86.641742],
+    [34.731177, -86.641775],
+    [34.731320, -86.641899]
+  ];
+
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221589685895278/IMG_2494.jpg?ex=6611ca3c&is=65ff553c&hm=cb52167d522d38e53c56a4f2456a77f3b66da66f7fb0fc288f03e94579334a3e&=&format=webp&width=583&height=778*/
+  const otherCtcTables = [
+    [34.730850, -86.640974],
+    [34.730904, -86.640805],
+    [34.730734, -86.640743],
+    [34.730649, -86.640970],
+    [34.730694, -86.641015]
+  ];
+
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221592659660810/IMG_2497.jpg?ex=6611ca3c&is=65ff553c&hm=c3f7acc7c9dbec2cfbb30da829e6e2ea9fbae5a8a71d8848840327a1cba52f29&=&format=webp&width=1038&height=778*/
+  const tableCourtyard = [
+    [34.730748, -86.640573],
+    [34.730778, -86.640342],
+    [34.730575, -86.640321],
+    [34.730561, -86.640582],
+    [34.730748, -86.640652]
+  ];
+
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221700365062204/IMG_2499.jpg?ex=6611ca56&is=65ff5556&hm=6caacbdd5758c9e661022cb9e69ccfd20ad941ae97bdf723270324006acc879c&=&format=webp&width=1038&height=778*/
+  const centralSand = [
+    [34.730256, -86.641688],
+    [34.730263, -86.641300],
+    [34.730059, -86.641292],
+    [34.730093, -86.641696]
+  ];
+
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221701690589256/IMG_2503.jpg?ex=6611ca56&is=65ff5556&hm=8a4fe62820c3ed7e93560dad88632a3808e0d9b7c4262585214ff27066ceb3a6&=&format=webp&width=1038&height=778*/
+  const i2cBench = [
+    [34.728710, -86.641824],
+    [34.728703, -86.642082],
+    [34.728647, -86.642068],
+    [34.728598, -86.641823]
+  ];
+
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221703204601948/IMG_2504.jpg?ex=6611ca57&is=65ff5557&hm=7a717f1113c52cab4574c17dabaf040c6dd871de716e2aea520f8d5d3a8a9e73&=&format=webp&width=1038&height=778*/
+  const babLit = [
+    [34.728557, -86.641828],
+    [34.728505, -86.642050],
+    [34.728404, -86.642028],
+    [34.728404, -86.641837]
+  ];
+
+  /* STOPPED POLYGONING FROM HERE*/
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221704437858334/IMG_2507.jpg?ex=6611ca57&is=65ff5557&hm=076918c483f3dc0488c68a31f3f937c5488931aff938587dbd55eb5d32badc91&=&format=webp&width=1038&height=778*/
+  const cguSlabs = [
+    [34.727284, -86.639439],
+    [34.727531, -86.639502],
+    [34.727493, -86.639384],
+    [34.727292, -86.639357]
+  ];
+
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221706962698340/IMG_2510.jpg?ex=6611ca58&is=65ff5558&hm=dbd5bdc6bd7ed784e68fc1b712424a01d6d9c2036664fb4874714b5b6571127c&=&format=webp&width=1038&height=778*/
+  const nearCrackerBarrel = [
+    [34.726796, -86.639665],
+    [34.726676, -86.639878],
+    [34.726778, -86.639974],
+    [34.726847, -86.639755]
+  ];
+
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221708883820634/IMG_2513.jpg?ex=6611ca58&is=65ff5558&hm=814ff0f58eab1f979cf9930463e6c3f0cf950795b230846c59e453f48a2573d8&=&format=webp&width=1038&height=778*/
+  const memoryCourtyard = [
+    [34.725546, -86.639769],
+    [34.725758, -86.639977],
+    [34.725758, -86.639556]
+  ];
+
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221710137786499/IMG_2514.jpg?ex=6611ca58&is=65ff5558&hm=0943d0e402f52ba06b72b62541b33e84086e42e1b308688bb203f5321e0d90fb&=&format=webp&width=1038&height=778*/
+  const swing1 = [
+    [34.725519, -86.639035],
+    [34.725410, -86.638861],
+    [34.725521, -86.638832]
+  ];
+
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221824839553134/IMG_2517.jpg?ex=6611ca74&is=65ff5574&hm=e758607de9a421d0b077ddd379191bcf767cc0e1a2ed9c15f9276f63288c701b&=&format=webp&width=1038&height=778*/
+  const swing2 = [
+    [34.725503, -86.638590],
+    [34.725484, -86.638464],
+    [34.725415, -86.638596],
+    [34.725417, -86.638467]
+  ];
+
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221817130160139/IMG_2515.jpg?ex=6611ca72&is=65ff5572&hm=921cf1cbd1ce949c06802057af0fc73d3c2adc5980e6805b21e3e902a67ad27c&=&format=webp&width=1038&height=778*/
+  const cvYard = [
+    [34.725234, -86.638509],
+    [34.725239, -86.638990],
+    [34.724992, -86.638987],
+    [34.724966, -86.638557]
+  ];
+
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221826437578893/IMG_2518.jpg?ex=6611ca74&is=65ff5574&hm=ce1854a5ef2157bd430a6112b7576390651fd19f8a09bec0b17ad108326c8b68&=&format=webp&width=1038&height=778*/
+  const hammock1 = [
+    [34.725322, -86.638419],
+    [34.725241, -86.638425],
+    [34.725244, -86.638537],
+    [34.725308, -86.638551]
+  ];
+
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221827498741862/IMG_2519.jpg?ex=6611ca74&is=65ff5574&hm=5ebbb4ea91f3422556975dbc3e7840d3771cfdf4170bd69daacce50a558f6c78&=&format=webp&width=1038&height=778*/
+  const bigCourtyard = [
+    [34.724754, -86.640004],
+    [34.724793, -86.639574],
+    [34.724361, -86.639586],
+    [34.724381, -86.640010]
+  ];
+
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221831072288788/IMG_2522.jpg?ex=6611ca75&is=65ff5575&hm=50610a57abc039fa440db2e024151c9cac707b538bf35b7f4c56eece7cdfe287&=&format=webp&width=1038&height=778*/
+  const egrCourtyard = [
+    [34.723350, -86.640031],
+    [34.723537, -86.639809],
+    [34.723389, -86.639650]
+  ];
+
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221897476378685/IMG_2530.jpg?ex=6611ca85&is=65ff5585&hm=8fa8d89ac7516fa292a0080fcd4e45e78a2d093186a66c9b5680901f0119edf5&=&format=webp&width=1038&height=778*/
+  const lakeSlab = [
+  [34.722477, -86.642788],
+  [34.722586, -86.642287],
+  [34.722087, -86.642340],
+  [34.722117, -86.642635]
+  ];
+
+  /*https://media.discordapp.net/attachments/1221136116518752367/1221221900244484227/IMG_2533.jpg?ex=6611ca86&is=65ff5586&hm=66c79c6cd6ee43082f870d7b18c692c71ea2a4e369ef8b5a8773473c6f2983c5&=&format=webp&width=1038&height=778*/
+  const shelbySlabs = [
+    [34.725281, -86.641545],
+    [34.725441, -86.641728],
+    [34.725521, -86.641429],
+    [34.725361, -86.641313]
   ];
 
     return(
@@ -163,40 +320,106 @@ export const Map = () => {
         <Popup>
           Test
         </Popup>
-        </Polygon>
+      </Polygon>
+
       {/* Greenhouse in SST*/}
       <Polygon color="green" positions={greenHouse} />
+
       {/*Tables near blackbox theatre*/}
       <Polygon color="green" positions={bbTheatre} />
+
       {/* Balcony on SST*/}
       <Polygon color="green" positions={sstBalcony} />
+
       {/* Morton Courtyard */}
       <Polygon color="green" positions={mortonCourt} />
+
       {/*Morton benches */}
       <Polygon color="green" positions={mortonBench} />
+
       {/*CU Lawn*/}
       <Polygon color="green" positions={cuLawn} />
+
       {/*Salmon Forest Hammock*/}
       <Polygon color="green" positions={salmonForest} />
+
       {/*Salmon Slab*/}
       <Polygon color="green" positions={salmonSlab} />
+
       {/*Nursing Benches*/}
       <Polygon color="green" positions={nursingBench} />
+
       {/*Salmon Bench 1*/}
       <Polygon color="green" positions={salmonBench1} />
+
       {/*Salmon Bench 2*/}
       <Polygon color="green" positions={salmonBench2} />
+
       {/*Art Structures metal*/}
       <Polygon color="green" positions={artStructures} />
+
       {/*Big tree behind the structures*/}
       <Polygon color="green" positions={bigTree} />
+
       {/*Frank Franz bench*/}
       <Polygon color="green" positions={franzBench} />
+
       {/*Slab in between Morton and CTC*/}
       <Polygon color="green" positions={mortonSlab} />
+
       {/*Old playground but now is only small gazebo and tables*/}
       <Polygon color="green" positions={oldPlayground} />
 
+      {/*Tables near CTC*/}
+      <Polygon color="green" positions={ctcTables} />
+
+      {/*The other tables near CTC*/}
+      <Polygon color="green" positions={otherCtcTables} />
+
+      {/* Cement courtyard of tables near the CTC but not too close into the trees*/}
+      <Polygon color="green" positions={tableCourtyard} />
+
+      {/*Sand volleyball courts in front of Central*/}
+      <Polygon color="green" positions={centralSand} />
+
+      {/*Benches in the entrance of I2C*/}
+      <Polygon color="green" positions={i2cBench} />
+
+      {/* BAB Lit up */}
+      <Polygon color="green" positions={babLit} />
+
+      {/* Slabs near CGU and crosswalk*/}
+      <Polygon color="green" positions={cguSlabs} />
+
+      {/* Near Cracker Barrel looking chairs*/}
+      <Polygon color="green" positions={nearCrackerBarrel} />
+
+      {/*Memorial Courtyard*/}
+      <Polygon color="green" positions={memoryCourtyard} />
+
+      {/*First swing near Chick fil A*/}
+      <Polygon color="green" positions={swing1} />
+
+      {/*Second swing near C Store*/}
+      <Polygon color="green" positions={swing2} />
+
+      {/*Charger Village Courtyard*/}
+      <Polygon color="green" positions={cvYard} />
+
+      {/*First hammock in CV courtyard*/}
+      <Polygon color="green" positions={hammock1} />
+
+      {/*Pillar Courtyard*/}
+      <Polygon color="green" positions={bigCourtyard} />
+
+      {/*Courtyard in front of EGR*/}
+      <Polygon color="green" positions={egrCourtyard} />
+
+      {/*Slab near the lake in front of Shelby*/}
+      <Polygon color="green" positions={lakeSlab} />
+
+      {/*Slabs in front of Shelby*/}
+      <Polygon color="green" positions={shelbySlabs} />
 
 
     </MapContainer>
